6813380986f2e7848dd92560e530fafa63e8572e
Brenda Wallace <brenda@wallace.net.nz>
2018-10-17 16:36:20 +1300

    Linter fix ups

--- openfisca_israel/variables/demographics.py
+++ openfisca_israel/variables/demographics.py
@@ -1,19 +1,15 @@
 # -*- coding: utf-8 -*-
-
-# This file defines variables for the modelled legislation.
-# A variable is a property of an Entity such as a Person, a Household…
-# See https://openfisca.org/doc/variables.html
-
-# Import from openfisca-core the common Python objects used to code the legislation in OpenFisca
 from openfisca_core.model_api import *
-# Import the Entities specifically defined for this tax and benefit system
 from openfisca_israel.entities import *
 
 
 # This variable is a pure input: it doesn't have a formula
 class birth(Variable):
     value_type = date
-    default_value = date(1970, 1, 1)  # By default, if no value is set for a simulation, we consider the people involved in a simulation to be born on the 1st of Jan 1970.
+    # By default, if no value is set for a simulation,
+    # we consider the people involved in a simulation
+    # to be born on the 1st of Jan 1970.
+    default_value = date(1970, 1, 1)
     entity = Person
     label = u"Birth date"
     definition_period = ETERNITY  # This variable cannot change over time.
@@ -54,4 +50,3 @@ class gender(Variable):
     entity = Person
     definition_period = MONTH
     label = u"A Person's gender"
-
